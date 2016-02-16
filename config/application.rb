cf9be5f1f1453e0af1a598b4d37edd872f4957b5
loremEc <milton@loremec.com>
2016-02-15 20:39:49 -0500

    aplicacion inicial

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module PaginaApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.secret_key_base = 'blipblapblup'
   end
 end
